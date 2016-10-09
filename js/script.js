2354646ae6742b1454cc3de0a9b851c7cc016a88
Andrey Krypskii <andreykrupskii@gmail.com>
2016-10-09 16:58:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	$(".page__menu").niceScroll({ 
+		cursorcolor: '#1bbc9b',
+		railoffset: {
+			left: -2
+		},
+		cursorborder: '0',
+		cursorwidth: '3px'
+	});
+});
